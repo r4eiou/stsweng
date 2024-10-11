536ce22c97f8ff81e3373f11a345bff6961358f2
Althea <garciareinaalthea@gmail.com>
2024-10-11 18:32:35 +0800

    Revert "Merge branch 'feature-signup_login_pages-unified_login/signup' into signup-login-backend"
    This reverts commit 1c9da1418e4a38c5aa4007432b4f0e5fa909ed86, reversing
    changes made to 08ca59cb383ee85735243e9fe01a9207db27e394.

--- public/js/login.js
+++ public/js/login.js
@@ -1,84 +1,83 @@
-/*
-document.addEventListener("DOMContentLoaded", function () {
-  var login_form = document.querySelector(".login-form");
+/*document.addEventListener("DOMContentLoaded", function() {
+    var login_form = document.querySelector(".login-form");
 
-  login_form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent default form submission
+    login_form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
 
-    // TODO: form validation here
+        // TODO: form validation here
+        
+        var selectedView = document.getElementById("login-title").innerHTML;
+        // var redirectUrl;
 
-    var selectedView = document.getElementById("login-title").innerHTML;
-    // var redirectUrl;
+        // console.log(selectedView);
 
-    // console.log(selectedView);
+        // const user_email = document.getElementById("email");
+        // const user_pw = document.getElementById("pw");
 
-    // const user_email = document.getElementById("email");
-    // const user_pw = document.getElementById("pw");
+        // user_email.classList.remove("input-error");
+        // user_pw.classList.remove("input-error");
 
-    // user_email.classList.remove("input-error");
-    // user_pw.classList.remove("input-error");
+        // let valid = true;
+        // let errorMessage = "";
+        
+        // if (!user_pw.value) {
+        //     valid = false;
+        //     user_pw.classList.add("input-error");
+        //     errorMessage += "Password is required.\n"
+        // }
+        // else {
+        //     user_pw.classList.remove("input-error");
+        // }
 
-    // let valid = true;
-    // let errorMessage = "";
+        // if (!user_email.value) {
+        //     valid = false;
+        //     user_email.classList.add("input-error");
+        //     errorMessage += "Email is required.\n"
+        // }
+        // else if (!user_email.value.includes("@")) {
+        //     valid = false;
+        //     user_email.classList.add("input-error");
+        //     errorMessage += "Invalid email.\n"
+        // }
+        // else {
+        //     user_email.classList.remove("input-error");
+        // }
 
-    // if (!user_pw.value) {
-    //     valid = false;
-    //     user_pw.classList.add("input-error");
-    //     errorMessage += "Password is required.\n"
-    // }
-    // else {
-    //     user_pw.classList.remove("input-error");
-    // }
-
-    // if (!user_email.value) {
-    //     valid = false;
-    //     user_email.classList.add("input-error");
-    //     errorMessage += "Email is required.\n"
-    // }
-    // else if (!user_email.value.includes("@")) {
-    //     valid = false;
-    //     user_email.classList.add("input-error");
-    //     errorMessage += "Invalid email.\n"
-    // }
-    // else {
-    //     user_email.classList.remove("input-error");
-    // }
-
-    if (true) {
-      switch (selectedView) {
-        case "Employee Log-in:":
-          //redirectUrl = "employee-homepage.html";
-          login("Employee");
-          break;
-        case "Administrator Log-in:":
-          //redirectUrl = "admin-homepage.html";
-          login("Admin");
-          break;
-        case "Tanod Log-in:":
-          //redirectUrl = "tanod-homepage.html";
-          login("Tanod");
-          break;
-        case "Lupon Log-in:":
-          //redirectUrl = "lupon-homepage.html";
-          login("Lupon");
-          break;
-        default:
-          //redirectUrl = "employee-homepage.html";
-          login("Employee");
-      }
-      login_form.submit();
-      //window.location.href = redirectUrl;
-    } else {
-      // showModal(errorMessage);
-    }
-  });
+        if(true) {
+            switch (selectedView) {
+                case "Employee Log-in:":
+                    //redirectUrl = "employee-homepage.html";
+                    login("Employee");
+                    break;
+                case "Administrator Log-in:":
+                    //redirectUrl = "admin-homepage.html";
+                    login("Admin");
+                    break;
+                case "Tanod Log-in:":
+                    //redirectUrl = "tanod-homepage.html";
+                    login("Tanod");
+                    break;
+                case "Lupon Log-in:":
+                    //redirectUrl = "lupon-homepage.html";
+                    login("Lupon");
+                    break;
+                default:
+                    //redirectUrl = "employee-homepage.html";
+                    login("Employee");
+            }
+                login_form.submit();
+            //window.location.href = redirectUrl;
+        } else {
+            // showModal(errorMessage);
+        }
+    });
 });
 
 // function showModal(message) {
 //     const modal = document.getElementById('validationModal');
 //     const modalMessage = document.getElementById('modalMessage');
 //     const closeBtn = document.getElementsByClassName('close')[0];
-
+    
 //     modalMessage.textContent = message;
 //     modalMessage.innerHTML = message.replace(/\n/g, '<br>');
 //     modal.style.display = 'block';
@@ -93,51 +92,52 @@ document.addEventListener("DOMContentLoaded", function () {
 //         }
 //     }
 // }
-
+    
 function tanodView() {
-  document.getElementById("emp-panel").style.background = "#AFE1D7";
-  document.getElementById("login-title").innerHTML = "Tanod Log-in:";
-  document.getElementById("login-title").style.color = "black";
-  document.getElementById("emp-lupon-tanod-btns").style.display = "none";
-  //  document.getElementById("submit-link").href = "tanod-homepage.html";
+    document.getElementById("emp-panel").style.background = "#AFE1D7";
+    document.getElementById("login-title").innerHTML = "Tanod Log-in:"
+    document.getElementById("login-title").style.color = "black";
+    document.getElementById("emp-lupon-tanod-btns").style.display = "none";
+   //  document.getElementById("submit-link").href = "tanod-homepage.html";
 }
 
 function luponView() {
-  document.getElementById("emp-panel").style.background = "#F3BE72";
-  document.getElementById("login-title").innerHTML = "Lupon Log-in:";
-  document.getElementById("login-title").style.color = "black";
-  document.getElementById("emp-lupon-tanod-btns").style.display = "none";
-  // document.getElementById("submit-link").href = "lupon-homepage.html";
+    document.getElementById("emp-panel").style.background = "#F3BE72";
+    document.getElementById("login-title").innerHTML = "Lupon Log-in:"
+    document.getElementById("login-title").style.color = "black";
+    document.getElementById("emp-lupon-tanod-btns").style.display = "none";
+    // document.getElementById("submit-link").href = "lupon-homepage.html";
 }
 
 function employeeView() {
-  if (document.getElementById("login-title").innerHTML == "Employee Log-in:") {
-    window.location.href = "../index.html";
-  } else {
-    document.getElementById("emp-panel").style.background = "#779FE5";
-    document.getElementById("login-title").innerHTML = "Employee Log-in:";
-    document.getElementById("login-title").style.color = "white";
-    document.getElementById("emp-lupon-tanod-btns").style.display = "flex";
-    // document.getElementById("submit-link").href = "employee-homepage.html";
-  }
+    if (document.getElementById("login-title").innerHTML == "Employee Log-in:") {
+        window.location.href = "../index.html";
+    }
+    else {
+        document.getElementById("emp-panel").style.background = "#779FE5";
+        document.getElementById("login-title").innerHTML = "Employee Log-in:"
+        document.getElementById("login-title").style.color = "white";
+        document.getElementById("emp-lupon-tanod-btns").style.display = "flex";
+        // document.getElementById("submit-link").href = "employee-homepage.html";
+    }
 }
 
 function login(userRole) {
-  localStorage.setItem("userRole", userRole);
-}
-
-function hidePW(signup_or_login) {
-  var x = document.getElementById(signup_or_login + "-pw");
-  var eye_icon = document.getElementById("eye-" + signup_or_login + "-pw");
-
-  // console.log(eye_icon)
-
-  if (x.type === "password") {
-    x.type = "text";
-    eye_icon.innerHTML = "<img src='/images/action-hide.png'>";
-  } else {
-    x.type = "password";
-    eye_icon.innerHTML = "<img src='/images/action-view.png'>";
-  }
+    localStorage.setItem('userRole', userRole);
 }
 */
+
+function hidePW(signup_or_login) {
+    var x = document.getElementById(signup_or_login + "-pw");
+    var eye_icon = document.getElementById("eye-" + signup_or_login + "-pw");
+  
+    // console.log(eye_icon)
+  
+    if (x.type === "password") {
+      x.type = "text";
+      eye_icon.innerHTML = "<img src='/images/action-hide.png'>";
+    } else {
+      x.type = "password";
+      eye_icon.innerHTML = "<img src='/images/action-view.png'>";
+    }
+  }
